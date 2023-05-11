e7b41f0478b4e695d42001e4465ef92a313969b5
Ajay Rathod <ajay.rathod@openxcell.com>
2023-05-11 18:58:07 +0530

    socekt io basic done

---
+++ Socket.controller.js
@@ -0,0 +1,29 @@
+const userArray = [];
+
+const addUser = ({ id, userName, chatRoomName }) => {
+  const name = userName.trim().toLowerCase();
+  const room = chatRoomName.trim().toLowerCase();
+
+  const isUserExist = userArray.find(
+    (user) => user.name === name && user.room === room
+  );
+
+  if (isUserExist) return { error: "UserName is taken, try with new userName" };
+
+  const user = { id, name, room };
+
+  userArray.push(user);
+
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = userArray.findIndex((user) => user.id === id);
+
+  if (index !== -1) return userArray.splice(index, 1)[0];
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+};
---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const chalk = require("chalk");
+const actuator = require("express-actuator");
+const socketIo = require("socket.io");
+
+/***************GLOBAL MIDDLEWARE***************/
+const PORT = process.env.PORT || 5050;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+app.use(cors());
+app.use(actuator({ infoGitMode: "full" }));
+const { addUser, removeUser } = require("./Socket.controller");
+
+/***************START SERVER***************/
+server.listen(PORT, () => {
+  console.log(
+    `server running on \n${chalk.underline.blue(`http://localhost:${PORT}`)}`
+  );
+});
+
+/***************SOCKET IO CONNECTION***************/
+io.on("connection", (socket) => {//start socket.io connection
+  console.log("🚀 ~ file: app.js:18 ~ socket.id:", socket.id);
+
+  //on use join start this
+  socket.on("join-room", ({ userName, chatRoomName }, callback) => {
+    const { error, user } = addUser({ id: socket.id, userName, chatRoomName });
+    console.log("🚀 ~ file: app.js:30 ~ socket.on ~ user:", user, error);
+
+    if (error) return callback(error);
+    //create a room in socket.io 
+    socket.join(chatRoomName.trim().toLowerCase());
+    //send welcome message for joining the room
+    socket.emit("chatMessage", `Welcome ${userName} to ${chatRoomName} room`);
+    //broadcast all other user that new user joined
+    socket.broadcast.emit("chatMessage", `${userName} has joined`);
+  });
+
+  socket.on("disconnect", () => {
+    const removedUser = removeUser(socket.id);
+    console.log("🚀 ~ file: app.js:35 ~ socket.on ~ removedUser:", removedUser);
+  });
+});
+
+/***************ROUTES***************/
+app.use("/", (req, res) => {
+  res.send("Server is running just fine");
+});
